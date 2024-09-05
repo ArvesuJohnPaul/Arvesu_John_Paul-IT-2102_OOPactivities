a2ef104fffa46c69f4c445f48f62b4affc2a8b28
ArvesuJohnPaul <23-05936@g.batstate-u.edu.ph>
2024-09-05 10:27:27 +0800

    GettingGreater.java

--- GettingGreater.java
+++ GettingGreater.java
@@ -6,15 +6,15 @@ public class GettingGreater{
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first character: ");
         char ch1 = scanner.next().charAt(0);
-        
+        int first = (int)ch1;
         System.out.print("Enter second character: ");
         char ch2 = scanner.next().charAt(0);
-
+        int second = (int)ch2;
         char maxChar = (char) Math.max(ch1, ch2);
         
         System.out.println("The character with the greater value is: " +maxChar);
-        System.out.println("Genre: " +ch1);
-        System.out.println("Album: " +ch2);
+        System.out.println("first: " +ch1);
+        System.out.println("second: " +ch2);
         
         scanner.close();
     
